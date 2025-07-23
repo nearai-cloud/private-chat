4629f4a34cf3c5226fbe6e5ffb398771fb206479
danielwpz <5139554+danielwpz@users.noreply.github.com>
2025-07-23 15:52:21 +0800

    try to run project

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,3 +1,4 @@
-export CORS_ALLOW_ORIGIN=http://localhost:5173/
+export CORS_ALLOW_ORIGIN=http://localhost:4173
+export DATABASE_URL=postgresql://backend:backend@localhost:5432/openwebui
 PORT="${PORT:-8080}"
 uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
