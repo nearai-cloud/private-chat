f67a4ad29f15b9222b2023cc1da9718a7efbbc42
danielwpz <5139554+danielwpz@users.noreply.github.com>
2025-07-26 10:45:32 +0800

    github oauth env

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,4 +1,9 @@
 export CORS_ALLOW_ORIGIN=http://localhost:4173
 export DATABASE_URL=postgresql://backend:backend@localhost:5432/openwebui
+
+export ENABLE_OAUTH_SIGNUP=true
+export GITHUB_CLIENT_ID=Ov23liaZf0YT5PyRIR4x
+# export GITHUB_CLIENT_SECRET=
+
 PORT="${PORT:-8080}"
 uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
