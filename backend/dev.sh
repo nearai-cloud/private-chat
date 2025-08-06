803733f1fa83ffa269dce82f70c79870bc2e2f21
danielwpz <5139554+danielwpz@users.noreply.github.com>
2025-08-06 10:42:16 +0800

    encrypt chat history

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,3 +1,7 @@
 export CORS_ALLOW_ORIGIN=http://localhost:5173
+export DATABASE_URL=postgresql://postgres:postgres@localhost:5432/openwebui
+export ENABLE_CHAT_ENCRYPTION=true
+export KEY_SERVICE_BASE_URL=https://b675e3b1f7ddff112ca2248523f184ac7ff698fa-3001.dstack-prod7.phala.network
+
 PORT="${PORT:-8080}"
 uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
