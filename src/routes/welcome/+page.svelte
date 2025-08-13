9109757b40b6f4ba7e89f62ae92a2509360bd99f
italic-jinxin <106428113+italic-jinxin@users.noreply.github.com>
2025-08-13 16:31:06 +0800

    feat: add page for unauth users

---
+++ src/routes/welcome/+page.svelte
@@ -0,0 +1,239 @@
+<script lang="ts">
+	import InputMenu from "$lib/components/chat/MessageInput/InputMenu.svelte";
+	import Selector from "$lib/components/chat/ModelSelector/Selector.svelte";
+	import Tooltip from "$lib/components/common/Tooltip.svelte";
+	import Bolt from "$lib/components/icons/Bolt.svelte";
+	import Headphone from "$lib/components/icons/Headphone.svelte";
+	import { removeModelPrefix } from "$lib/utils/model";
+	import { getContext } from "svelte";
+	import Fuse from "fuse.js";
+	import { allPrompts, models } from "./data";
+	import { arraysEqual } from "./utils";
+	import { goto } from "$app/navigation";
+
+    const i18n: any = getContext('i18n');
+    
+    let selectedModel: any = models[0].id
+    let inputValue = ''
+
+    let filteredPrompts: any[] = [];
+    const getFilteredPrompts = (inputValue: string, model: string) => {
+        const sortedPrompts = [...(allPrompts ?? [])].sort(() => Math.random() - 0.5);
+		if (inputValue.length > 500) {
+			filteredPrompts = [];
+		} else {
+            const fuse = new Fuse(sortedPrompts, {
+                keys: ['content', 'title'],
+                threshold: 0.5
+            });
+			const newFilteredPrompts =
+				inputValue.trim() && fuse
+					? fuse.search(inputValue.trim()).map((result) => result.item)
+					: sortedPrompts;
+
+			if (!arraysEqual(filteredPrompts, newFilteredPrompts)) {
+				filteredPrompts = newFilteredPrompts;
+			}
+		}
+	};
+    $: getFilteredPrompts(inputValue, selectedModel);
+
+    const gotoAuth = async () => {
+        await goto(`/auth`)
+    }
+</script>
+
+<div class="h-screen max-h-[100dvh] w-full max-w-full flex flex-col">
+    <!-- model selector -->
+    <div class="flex flex-col w-full items-start absolute left-4 top-4">
+        <div class="flex w-full max-w-fit">
+            <div class="max-w-full m-1">
+                <Selector
+                    placeholder={$i18n.t('Select a model')}
+                    items={models.map((model) => ({
+                        value: model.id,
+                        label: removeModelPrefix(model.name),
+                        model: model
+                    }))}
+                    bind:value={selectedModel}
+                />
+            </div>
+        </div>
+    </div>
+
+    <div class="w-full h-full flex flex-col">
+        <div class="m-auto w-full max-w-6xl px-2 2xl:px-20 translate-y-6 py-24 text-center">
+            <div class="w-full text-3xl text-gray-800 dark:text-gray-100 text-center flex items-center gap-4 font-primary">
+                <div class="w-full flex flex-col justify-center items-center">
+                    <!-- title -->
+                    <div class="flex flex-row justify-center gap-3 items-center sm:gap-3.5 w-fit px-5 max-w-xl">
+                        <img class="size-9 sm:size-10 rounded-full border-[1px] border-gray-100 dark:border-none" src="/static/favicon.png" alt="">
+                        <div class="text-3xl sm:text-3xl line-clamp-1 flex items-center">
+                            <span class="line-clamp-1">{selectedModel}</span>
+                        </div>
+                    </div>
+                    <!-- input -->
+                    <div class="text-base font-normal md:max-w-3xl w-full py-3">
+                        <div class="w-full font-primary bg-white dark:bg-gray-900 px-2.5 mx-auto inset-x-0">
+                            <div class="flex-1 flex flex-col relative w-full shadow-lg rounded-3xl border border-gray-50 dark:border-gray-850 hover:border-gray-100 focus-within:border-gray-100 hover:dark:border-gray-800 focus-within:dark:border-gray-800 transition px-1 bg-white/90 dark:bg-gray-400/5 dark:text-gray-100">
+                                <div class="px-2.5">
+                                    <div
+                                        class="scrollbar-hidden rtl:text-right ltr:text-left bg-transparent dark:text-gray-100 outline-hidden w-full pt-2.5 pb-[5px] px-1 resize-none h-fit max-h-80 overflow-auto"
+                                        id="chat-input-container"
+                                    >
+                                        <textarea
+                                            class="scrollbar-hidden bg-transparent dark:text-gray-200 outline-hidden w-full pt-3 px-1 resize-none"
+                                            placeholder={$i18n.t('Send a Message')}
+                                            rows="1"
+                                            bind:value={inputValue}
+                                            on:keydown={async (e) => {
+                                                const isCtrlPressed = e.ctrlKey || e.metaKey
+                                                const enterPressed = (e.key === 'Enter' || e.keyCode === 13) && isCtrlPressed
+                                                if (enterPressed) {
+                                                    gotoAuth()
+                                                }
+                                            }}
+                                        />
+                                    </div>
+                                    <div class=" flex justify-between mt-0.5 mb-2.5 mx-0.5 max-w-full" dir="ltr">
+                                        <div class="ml-1 self-end flex items-center flex-1 max-w-[80%]">
+                                            <InputMenu
+                                                inputFilesHandler={() => {}}
+                                                screenCaptureHandler={() => {}}
+                                                uploadFilesHandler={() => {}}
+                                                uploadGoogleDriveHandler={() => {}}
+                                                uploadOneDriveHandler={() => {}}
+                                                onClose={() => {}}
+										    >
+                                                <div
+                                                    class="bg-transparent hover:bg-gray-100 text-gray-800 dark:text-white dark:hover:bg-gray-800 rounded-full p-1.5 outline-hidden focus:outline-hidden"
+                                                >
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        viewBox="0 0 20 20"
+                                                        aria-hidden="true"
+                                                        fill="currentColor"
+                                                        class="size-5"
+                                                    >
+                                                        <path
+                                                            d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z"
+                                                        />
+                                                    </svg>
+                                                </div>
+                                            </InputMenu>
+                                        </div>
+
+                                        <div class="self-end flex space-x-1 mr-1 shrink-0">
+                                            <Tooltip content={$i18n.t('Dictate')}>
+												<button
+													id="voice-input-button"
+													class=" text-gray-600 dark:text-gray-300 hover:text-gray-700 dark:hover:text-gray-200 transition rounded-full p-1.5 mr-0.5 self-center"
+													type="button"
+													on:click={gotoAuth}
+													aria-label="Voice Input"
+												>
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 20 20"
+														fill="currentColor"
+														class="w-5 h-5 translate-y-[0.5px]"
+													>
+														<path d="M7 4a3 3 0 016 0v6a3 3 0 11-6 0V4z" />
+														<path
+															d="M5.5 9.643a.75.75 0 00-1.5 0V10c0 3.06 2.29 5.585 5.25 5.954V17.5h-1.5a.75.75 0 000 1.5h4.5a.75.75 0 000-1.5h-1.5v-1.546A6.001 6.001 0 0016 10v-.357a.75.75 0 00-1.5 0V10a4.5 4.5 0 01-9 0v-.357z"
+														/>
+													</svg>
+												</button>
+											</Tooltip>
+                                            <div class=" flex items-center">
+                                                <Tooltip content={$i18n.t('Voice mode')}>
+													<button
+														class=" bg-black text-white hover:bg-gray-900 dark:bg-white dark:text-black dark:hover:bg-gray-100 transition rounded-full p-1.5 self-center"
+														type="button"
+														on:click={gotoAuth}
+														aria-label={$i18n.t('Voice mode')}
+													>
+														<Headphone className="size-5" />
+													</button>
+												</Tooltip>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- suggestion -->
+                    <div class="mx-auto max-w-2xl w-full font-primary mt-2">
+                        <div class="mx-5">
+                            <div class="mb-1 flex gap-1 text-xs font-medium items-center text-gray-600 dark:text-gray-400">
+                                <Bolt />
+		                        {$i18n.t('Suggested')}
+                            </div>
+                            <div class="h-40 w-full">
+                                <div role="list" class="max-h-40 overflow-auto scrollbar-none items-start">
+                                    {#each filteredPrompts as prompt, idx (prompt.id || prompt.content)}
+                                        <!-- svelte-ignore a11y-no-interactive-element-to-noninteractive-role -->
+                                        <button
+                                            role="listitem"
+                                            class="waterfall flex flex-col flex-1 shrink-0 w-full justify-between
+                                            px-3 py-2 rounded-xl bg-transparent hover:bg-black/5 text-base font-normal
+                                            dark:hover:bg-white/5 transition group"
+                                            style="animation-delay: {idx * 60}ms"
+                                            on:click={() => {
+                                                inputValue = prompt.content;
+                                            }}
+                                        >
+                                            <div class="flex flex-col text-left">
+                                                {#if prompt.title && prompt.title[0] !== ''}
+                                                    <div
+                                                        class="font-medium dark:text-gray-300 dark:group-hover:text-gray-200 transition line-clamp-1"
+                                                    >
+                                                        {prompt.title[0]}
+                                                    </div>
+                                                    <div class="text-xs text-gray-600 dark:text-gray-400 font-normal line-clamp-1">
+                                                        {prompt.title[1]}
+                                                    </div>
+                                                {:else}
+                                                    <div
+                                                        class="font-medium dark:text-gray-300 dark:group-hover:text-gray-200 transition line-clamp-1"
+                                                    >
+                                                        {prompt.content}
+                                                    </div>
+                                                    <div class="text-xs text-gray-600 dark:text-gray-400 font-normal line-clamp-1">
+                                                        {$i18n.t('Prompt')}
+                                                    </div>
+                                                {/if}
+                                            </div>
+                                        </button>
+                                    {/each}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+	@keyframes fadeInUp {
+		0% {
+			opacity: 0;
+			transform: translateY(20px);
+		}
+		100% {
+			opacity: 1;
+			transform: translateY(0);
+		}
+	}
+
+	.waterfall {
+		opacity: 0;
+		animation-name: fadeInUp;
+		animation-duration: 200ms;
+		animation-fill-mode: forwards;
+		animation-timing-function: ease;
+	}
+</style>
---
+++ src/routes/welcome/data.ts
@@ -0,0 +1,112 @@
+export const models = [
+	{
+		id: 'llama-3.3-70b-instruct',
+		object: 'model',
+		created: 1677610602,
+		owned_by: 'openai',
+		openai: {
+			id: 'llama-3.3-70b-instruct'
+		},
+		urlIdx: '0',
+		tags: [],
+		name: 'llama-3.3-70b-instruct',
+		direct: true
+	},
+	{
+		id: 'deepseek-chat-v3-0324',
+		object: 'model',
+		created: 1677610602,
+		owned_by: 'openai',
+		openai: {
+			id: 'deepseek-chat-v3-0324'
+		},
+		urlIdx: '0',
+		tags: [],
+		name: 'deepseek-chat-v3-0324',
+		direct: true
+	},
+	{
+		id: 'gpt-oss-120b',
+		object: 'model',
+		created: 1677610602,
+		owned_by: 'openai',
+		openai: {
+			id: 'gpt-oss-120b'
+		},
+		urlIdx: '0',
+		tags: [],
+		name: 'gpt-oss-120b',
+		direct: true
+	},
+	{
+		id: 'qwen3-coder',
+		object: 'model',
+		created: 1677610602,
+		owned_by: 'openai',
+		openai: {
+			id: 'qwen3-coder'
+		},
+		urlIdx: '0',
+		tags: [],
+		name: 'qwen3-coder',
+		direct: true
+	},
+	{
+		id: 'qwen-2.5-7b-instruct',
+		object: 'model',
+		created: 1677610602,
+		owned_by: 'openai',
+		openai: {
+			id: 'qwen-2.5-7b-instruct'
+		},
+		urlIdx: '0',
+		tags: [],
+		name: 'qwen-2.5-7b-instruct',
+		direct: true
+	},
+	{
+		id: 'qwen2.5-vl-72b-instruct',
+		object: 'model',
+		created: 1677610602,
+		owned_by: 'openai',
+		openai: {
+			id: 'qwen2.5-vl-72b-instruct'
+		},
+		urlIdx: '0',
+		tags: [],
+		name: 'qwen2.5-vl-72b-instruct',
+		direct: true
+	}
+];
+
+
+export const allPrompts = [
+	{
+		title: ['Help me study', 'vocabulary for a college entrance exam'],
+		content:
+			"Help me study vocabulary: write a sentence for me to fill in the blank, and I'll try to pick the correct option."
+	},
+	{
+		title: ['Give me ideas', "for what to do with my kids' art"],
+		content:
+			"What are 5 creative things I could do with my kids' art? I don't want to throw them away, but it's also so much clutter."
+	},
+	{
+		title: ['Tell me a fun fact', 'about the Roman Empire'],
+		content: 'Tell me a random fun fact about the Roman Empire'
+	},
+	{
+		title: ['Show me a code snippet', "of a website's sticky header"],
+		content: "Show me a code snippet of a website's sticky header in CSS and JavaScript."
+	},
+	{
+		title: ['Explain options trading', "if I'm familiar with buying and selling stocks"],
+		content:
+			"Explain options trading in simple terms if I'm familiar with buying and selling stocks."
+	},
+	{
+		title: ['Overcome procrastination', 'give me tips'],
+		content:
+			'Could you start by asking me about instances when I procrastinate the most and then give me some suggestions to overcome it?'
+	}
+];
