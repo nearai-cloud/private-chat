c33ed069f91a373c5ec82ff9d102c47c11765497
italic-jinxin <106428113+italic-jinxin@users.noreply.github.com>
2025-08-14 20:00:06 +0800

    feat: Add button for sending messages

--- src/routes/welcome/+page.svelte
+++ src/routes/welcome/+page.svelte
@@ -145,18 +145,44 @@
 													</svg>
 												</button>
 											</Tooltip>
-                                            <div class=" flex items-center">
-                                                <Tooltip content={$i18n.t('Voice mode')}>
-													<button
-														class=" bg-black text-white hover:bg-gray-900 dark:bg-white dark:text-black dark:hover:bg-gray-100 transition rounded-full p-1.5 self-center"
-														type="button"
-														on:click={gotoAuth}
-														aria-label={$i18n.t('Voice mode')}
-													>
-														<Headphone className="size-5" />
-													</button>
-												</Tooltip>
-                                            </div>
+                                            {#if inputValue === '' }
+                                                <div class="flex items-center">
+                                                    <Tooltip content={$i18n.t('Voice mode')}>
+                                                        <button
+                                                            class="bg-black text-white hover:bg-gray-900 dark:bg-white dark:text-black dark:hover:bg-gray-100 transition rounded-full p-1.5 self-center"
+                                                            type="button"
+                                                            on:click={gotoAuth}
+                                                            aria-label={$i18n.t('Voice mode')}
+                                                        >
+                                                            <Headphone className="size-5" />
+                                                        </button>
+                                                    </Tooltip>
+                                                </div>
+                                            {:else}
+                                                <div class="flex items-center">
+                                                    <Tooltip content={$i18n.t('Send message')}>
+                                                        <button
+                                                            class="bg-black text-white hover:bg-gray-900 dark:bg-white dark:text-black dark:hover:bg-gray-100 transition rounded-full p-1.5 self-center"
+                                                            type="button"
+                                                            on:click={gotoAuth}
+                                                            aria-label={$i18n.t('Send message')}
+                                                        >
+                                                            <svg
+															xmlns="http://www.w3.org/2000/svg"
+															viewBox="0 0 16 16"
+															fill="currentColor"
+															class="size-5"
+														>
+															<path
+																fill-rule="evenodd"
+																d="M8 14a.75.75 0 0 1-.75-.75V4.56L4.03 7.78a.75.75 0 0 1-1.06-1.06l4.5-4.5a.75.75 0 0 1 1.06 0l4.5 4.5a.75.75 0 0 1-1.06 1.06L8.75 4.56v8.69A.75.75 0 0 1 8 14Z"
+																clip-rule="evenodd"
+															/>
+														</svg>
+                                                        </button>
+                                                    </Tooltip>
+                                                </div>
+                                            {/if}
                                         </div>
                                     </div>
                                 </div>
