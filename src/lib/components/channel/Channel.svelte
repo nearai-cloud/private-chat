19706dc218fb5326078215ce6be6d686061d6515
jinmax2024 <106428113+italic-jinxin@users.noreply.github.com>
2025-08-20 12:11:53 +0800

    feat: update setting about page

--- src/lib/components/channel/Channel.svelte
+++ src/lib/components/channel/Channel.svelte
@@ -195,7 +195,7 @@
 </script>
 
 <svelte:head>
-	<title>#{channel?.name ?? 'Channel'} | Open WebUI</title>
+	<title>#{channel?.name ?? 'Channel'} | NEAR AI Private Chat</title>
 </svelte:head>
 
 <div
