8ded60fb14e6c26bfb61b67bd3fa71ca4342ca6f
italic-jinxin <106428113+italic-jinxin@users.noreply.github.com>
2025-08-27 19:24:40 +0800

    chore: format

--- src/lib/components/chat/VerifySignatureDialog.svelte
+++ src/lib/components/chat/VerifySignatureDialog.svelte
@@ -64,7 +64,11 @@
 					<div
 						class="mb-4 py-2 px-2.5 text-green-700 dark:text-green-300 bg-green-50 dark:bg-green-900/20 border border-green-200 dark:border-green-800 rounded-lg text-sm"
 					>
-						<svg class="w-5 -mt-[2px] h-5 text-green-500 mr-0.5 inline-block" fill="currentColor" viewBox="0 0 20 20">
+						<svg
+							class="w-5 -mt-[2px] h-5 text-green-500 mr-0.5 inline-block"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+						>
 							<path
 								fill-rule="evenodd"
 								d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
