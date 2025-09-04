5c1022214fff781a8c97b364247a5b723d8cdcc9
danielwpz <5139554+danielwpz@users.noreply.github.com>
2025-09-04 15:01:45 +0800

    update KEY_FILE path

--- backend/start.sh
+++ backend/start.sh
@@ -14,7 +14,7 @@ if [[ "${WEB_LOADER_ENGINE,,}" == "playwright" ]]; then
     python -c "import nltk; nltk.download('punkt_tab')"
 fi
 
-KEY_FILE=.webui_secret_key
+KEY_FILE=data/.webui_secret_key
 
 PORT="${PORT:-8080}"
 HOST="${HOST:-0.0.0.0}"
