bd262066ca64ba97ee7e2c4d6ecd306e906378ff
jinmax2024 <106428113+italic-jinxin@users.noreply.github.com>
2025-09-08 16:37:57 +0800

    chore: minor

--- src/routes/terms/+page.svelte
+++ src/routes/terms/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-    import PDFViewer from '$lib/components/PDFViewer.svelte';
+	import PDFViewer from '$lib/components/PDFViewer.svelte';
 
-    const termsUrl = '/terms.pdf';
+	const termsUrl = '/terms.pdf';
 </script>
 
-<div  class="h-screen max-h-[100dvh] text-gray-700 overflow-scroll dark:text-gray-100 bg-white dark:bg-gray-900 w-full max-w-full flex flex-col">
-    <PDFViewer pdfUrl={termsUrl} />
+<div
+	class="h-screen max-h-[100dvh] text-gray-700 overflow-scroll dark:text-gray-100 bg-white dark:bg-gray-900 w-full max-w-full flex flex-col"
+>
+	<PDFViewer pdfUrl={termsUrl} />
 </div>
