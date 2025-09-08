60f1cde61a4a9b7226d2818b58d52f4a74cb6ec3
jinxin <106428113+italic-jinxin@users.noreply.github.com>
2025-09-08 15:47:55 +0800

    feat: optimize initial chat loading performance (#59)

--- src/lib/apis/tools/index.ts
+++ src/lib/apis/tools/index.ts
@@ -31,35 +31,37 @@ export const createNewTool = async (token: string, tool: object) => {
 	return res;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getTools = async (token: string = '') => {
-	let error = null;
+	return [];
+	// let error = null;
 
-	const res = await fetch(`${WEBUI_API_BASE_URL}/tools/`, {
-		method: 'GET',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			authorization: `Bearer ${token}`
-		}
-	})
-		.then(async (res) => {
-			if (!res.ok) throw await res.json();
-			return res.json();
-		})
-		.then((json) => {
-			return json;
-		})
-		.catch((err) => {
-			error = err.detail;
-			console.log(err);
-			return null;
-		});
+	// const res = await fetch(`${WEBUI_API_BASE_URL}/tools/`, {
+	// 	method: 'GET',
+	// 	headers: {
+	// 		Accept: 'application/json',
+	// 		'Content-Type': 'application/json',
+	// 		authorization: `Bearer ${token}`
+	// 	}
+	// })
+	// 	.then(async (res) => {
+	// 		if (!res.ok) throw await res.json();
+	// 		return res.json();
+	// 	})
+	// 	.then((json) => {
+	// 		return json;
+	// 	})
+	// 	.catch((err) => {
+	// 		error = err.detail;
+	// 		console.log(err);
+	// 		return null;
+	// 	});
 
-	if (error) {
-		throw error;
-	}
+	// if (error) {
+	// 	throw error;
+	// }
 
-	return res;
+	// return res;
 };
 
 export const getToolList = async (token: string = '') => {
