7c54af71690743ff90e1d0f5e88eaf010b2c11dc
danielwpz <5139554+danielwpz@users.noreply.github.com>
2025-09-15 17:55:51 +0800

    revert comment

--- backend/open_webui/utils/chat_encryption_proxy.py
+++ backend/open_webui/utils/chat_encryption_proxy.py
@@ -43,7 +43,7 @@ class ChatTableEncryptionProxy(ChatTable):
         )
 
     def _create_chat_model_from_db_record(self, db_record) -> ChatModel:
-        """Create ChatModel from database record, handling encrypted data now."""
+        """Create ChatModel from database record, handling encrypted data."""
         chat_data = db_record.chat
         title_data = db_record.title
 
